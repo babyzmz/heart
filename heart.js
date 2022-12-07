1e8bb72a41198f4e77ad4563ad56a2ad2affa939
babyzmz <111495735+babyzmz@users.noreply.github.com>
2022-12-08 07:26:54 +1100

    Add files via upload

---
+++ heart.js
@@ -0,0 +1,11 @@
+function sl() {
+    var div = document.getElementsByClassName("text")[0];
+    
+    if (div.style.display == "block") {
+        div.style.display = "none";
+        
+    } else {
+        div.style.display = "block";
+    }
+    
+}
